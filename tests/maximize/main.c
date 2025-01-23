1a43112165d1b8cd555075da6d58b535c56935bb
Lukas <lukas.hertel@tum.de>
2025-01-23 15:37:21 +0100

    This change was wrongly made in an earlier commit

--- tests/maximize/main.c
+++ tests/maximize/main.c
@@ -8,28 +8,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-size_t minimize(void *sym_var, size_t length) { exit(1); }
+size_t maximize(void *sym_var, size_t length) { exit(1); }
 void *new_sym_var(size_t length) { exit(1); }
 
 int main() {
   void *sym_var = new_sym_var(64);
 
   // Unconstrained
-  printf("%zx", minimize(sym_var, 64));
+  printf("%zx", maximize(sym_var, 64));
 
   // Else if, so we do not analyze combinations of if statements
   if (*(size_t *)sym_var == 0) {
     // Here, we can only have the solution 0
-    printf("%zx", minimize(sym_var, 64));
+    printf("%zx", maximize(sym_var, 64));
   } else if ((*(size_t *)sym_var) * 2 < 1) {
     // This returns 0x8000000000000000, as 2 * that = 0
-    printf("%zx", minimize(sym_var, 64));
+    printf("%zx", maximize(sym_var, 64));
   } else if (*(size_t *)sym_var < 0x7FFFFFFF) {
     // And step through a couple of adjacent numbers
-    printf("%zx", minimize(sym_var, 64));
+    printf("%zx", maximize(sym_var, 64));
   } else if (*(size_t *)sym_var < 0x80000000) {
-    printf("%zx", minimize(sym_var, 64));
+    printf("%zx", maximize(sym_var, 64));
   } else if (*(size_t *)sym_var < 0x80000001) {
-    printf("%zx", minimize(sym_var, 64));
+    printf("%zx", maximize(sym_var, 64));
   }
 }
